b4127070df4cd2d9adee5e1f37941d0062000d64
Matt Stone <matthieu.stone@gmail.com>
2020-12-15 17:28:07 +1100

    Recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 # https://github.com/ambethia/recaptcha#alternative-api-key-setup
 Recaptcha.configure do |config|
-  config.site_key  = '6LfCM9sZAAAAACAWaELXiAVdLUP-wOJjOJsBopY7'
-  config.secret_key = '6LfCM9sZAAAAACDMSt8-4DkY7ZCD5-Q6g9tfPjTq'
+  config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
