f5f0bc632bbafddb85ecea938b205c1230f8b2bb
Matt Stone <matthieu.stone@gmail.com>
2020-12-22 14:28:58 +1100

    Contact form now sends emails to hello@mybond.com.au

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -18,16 +18,37 @@ class IndexController < ApplicationController
   def contact
     log_header
 
+
     @name    = params[:name]
     @email   = params[:email]
     @mobile  = params[:mobile]
     @message = params[:message]
 
-    if !@name.nil?
+    @error = case
+      when params[:name].blank?   then "Please provide your name"
+      when params[:email].blank?  then "Please provide your email"
+      when !valid_email?          then "Please provide a valid email"
+      when params[:mobile].blank? then "Please provide a mobile number"
+      when prams[:message].blank? then "Please provide a message"
+      else nil
+      end
+
+    case @error.nil?
+    when false then flash[:error] = @error
+    when true
       # send email to... contact@mybond.com.au
       flash[:notice] = "Thank you. We will respond to you as soon as we can."
     end
 
+  end
+
+  private
+
+  def email_valid?
+    case params[:email] =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    when nil then false
+    when 0   then true
+    end
   end
 
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,16 @@
 class ContactMailer < ApplicationMailer
-  default from: "contact@mybond.com.au"
+  default from: "hello@mybond.com.au"
+
+
+  def contact(to, message)
+    @message = message
+
+    # mail(from: "contact@mybond.com.au", to: to, subject: "Contact Notification") do |format|
+    mail(to: to, subject: "Contact Notification") do |format|
+      format.html { render "contact" }
+      format.text { render plain: 'Unable to render.' }
+    end
+  end
 
 
 end
